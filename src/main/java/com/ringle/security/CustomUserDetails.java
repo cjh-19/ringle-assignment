1c9433b1fdd6c351a16aec26849f5bf1846bcb2e
cjh-19 <gns8812@naver.com>
2025-03-26 01:35:17 +0900

    feat: UserDetails 커스터마이징

---
+++ src/main/java/com/ringle/security/CustomUserDetails.java
@@ -0,0 +1,31 @@
+package com.ringle.security;
+
+import com.ringle.domain.user.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "ROLE_" + user.getRole().name());
+    }
+
+    @Override public String getPassword() { return user.getPassword(); }
+    @Override public String getUsername() { return user.getEmail(); }
+}
---
+++ src/main/java/com/ringle/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.ringle.security;
+
+import com.ringle.domain.user.entity.User;
+import com.ringle.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) throw new UsernameNotFoundException("등록된 사용자가 없습니다.");
+        return new CustomUserDetails(user);
+    }
+}
+

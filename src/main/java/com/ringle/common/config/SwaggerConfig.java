572be9c55f9bcc97671949e20cad49133dfdcd48
cjh-19 <gns8812@naver.com>
2025-03-26 02:46:47 +0900

    feat: Swagger 문서화 설정(security 적용)

---
+++ src/main/java/com/ringle/common/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.ringle.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        SecurityScheme securityScheme = getSecurityScheme();
+        SecurityRequirement securityRequirement = getSecurityRequireMent();
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(List.of(securityRequirement))
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("링글 코딩 과제")
+                .description("링글 수강 신청 REST API")
+                .version("1.0.0");
+    }
+
+    private SecurityScheme getSecurityScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+    }
+
+    private SecurityRequirement getSecurityRequireMent() {
+        return new SecurityRequirement().addList("bearerAuth");
+    }
+}
